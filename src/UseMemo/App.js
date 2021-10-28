8a7fa07e97734e2b4a3d29c5180d10fd9a129530
Norulshahlam <norulshahlam@gmail.com>
2021-10-29 00:52:31 +0800

    useMemo

--- src/UseMemo/App.js
+++ src/UseMemo/App.js
@@ -1,13 +1,18 @@
 /*
 useMemo
 
-in this example, we have 2 identical Child component and when a state changes, this component gets re-rendered. We can find out by using useEffect on the child component and set a counter to it.
+Example
+We have 2 identical Child component and when a state changes, both component should get re-rendered. We can find out by using useEffect on the child component and set a counter to it.
 
 Now by using useMemo and using empty array as its dependency, it wont re-redner this component, unless the dependency changes. The configuration is similar to useEffect
 
-by using useMemo, it checks for a state defined (number input for this case), n if the number doesnt change, it will skip this method
+By using useMemo, it checks for a state defined (number input for this case), n if the number doesnt change, it will skip this method
 
 useMemo will only recompute the memorized value when one of the dependencies has changed. This optimization helps to avoid expensive calculations on every render.
+
+When should I use?
+
+useMemo should be used when there is a high amount of processing. The threshold from when useMemo becomes interesting for avoiding extra processing highly depends on your application. Using useMemo in cases with very low processing, there can be extra overhead for its usage.
 
 Should I use useMemo everywhere?
 Why not use useMemo everywhere then? In short, it's not a free performance optimisation. There's an additional cost (memory usage, for one) incurred when setting up useMemo , that can very quickly outweigh the performance benefit of remembering every single function's possible value.
